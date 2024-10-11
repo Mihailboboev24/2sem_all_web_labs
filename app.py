68e7bcf345cf1d65f72c59b530bdf33e506495f4
Mihailboboev24 <160295013+Mihailboboev24@users.noreply.github.com>
2024-10-12 04:36:41 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -202,7 +202,9 @@ def example():
     ]
     return render_template('example.html', name=name, group_student=group_student, number_course=number_course, fruits=fruits)
 
-
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
     
 if __name__ == "__main__":
     app.run(debug=False)
