37215ee92afae9bd4ec3715ae5d9efa2844f4d26
Mihailboboev24 <160295013+Mihailboboev24@users.noreply.github.com>
2024-10-12 02:36:29 +0700

    имя студента

--- app.py
+++ app.py
@@ -192,7 +192,8 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
-
+    name = 'Бобоев Михаил'
+    return render_template('example.html', name=name)
+    
 if __name__ == "__main__":
     app.run(debug=True)
