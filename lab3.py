86753a076de42ea9f92c6e5fe4c64d91ba0f973d
SergoSektor <serg.sektor.droid@gmail.com>
2024-11-09 03:00:28 +0700

    корректировка пола

--- lab3.py
+++ lab3.py
@@ -5,7 +5,7 @@ lab3 = Blueprint('lab3', __name__)
 def lab():
     name = request.cookies.get('name')
     name_color = request.cookies.get('name_color')
-    return render_template('lab3.html', name=name, name_color=name_color)
+    return render_template('lab3/lab3.html', name=name, name_color=name_color)
 @lab3.route('/lab3/cookie')
 def cookie():
     resp = make_response(redirect('/lab3/'))
@@ -20,3 +20,14 @@ def del_cookie():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp
+@lab3.route('/lab3/form1')
+def form1():
+    errors = {}
+    user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+    age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
