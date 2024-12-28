2e18505d439bc4f7d6138f8e54dd3c66d857a2eb
Mihailboboev24 <mikhail.boboev@mail.ru>
2024-12-28 21:58:59 +0700

    Дополнительное задание

--- db/models.py
+++ db/models.py
@@ -11,3 +11,5 @@ class articles(db.Model):
     is_favorite = db.Column(db.Boolean)
     is_public = db.Column(db.Boolean)
     likes = db.Column(db.Integer)
+    # Связь с пользователем
+    user = db.relationship('users', backref=db.backref('articles', lazy=True))
