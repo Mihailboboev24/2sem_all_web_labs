dc56222b66bc3953b29ce3a5202cc7e580ac34af
Mihailboboev24 <mikhail.boboev@mail.ru>
2024-12-28 22:18:45 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from lab6 import lab6
 from lab6 import populate_offices
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app = Flask(__name__)
 
@@ -55,6 +56,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.route("/")
 @app.route("/index")
@@ -108,6 +110,9 @@ def menu():
                     </li>
                     <li>
                         <a href="/lab8">Восьмая лабораторная</a>
+                    </li>
+                    <li>
+                        <a href="/lab9">Девятая лабораторная</a>
                     </li>
                     <li>
                         <a href="/lab1/student">Студент</a>
